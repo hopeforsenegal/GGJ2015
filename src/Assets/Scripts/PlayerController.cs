e1f9ce752f2226ef50209809e1b90bc93b29d680
kkdaman <kkdaman@hotmail.com>
2015-01-24 19:56:48 -0600

    Adding skeleton for PvP Collision

--- src/Assets/Scripts/PlayerController.cs
+++ src/Assets/Scripts/PlayerController.cs
@@ -4,28 +4,30 @@ using System.Collections;
 [RequireComponent(typeof(PlayerMovement))]
 public class PlayerController : MonoBehaviour
 {
+    private const string SpritePath = "SpriteAnchor/Sprite";
+
     void Update()
     {
         PlayerMovement movement = this.GetComponent<PlayerMovement>();
+        Animator animator = this.GetComponentInChildren<Animator>();
+
         if (movement.tag.CompareTo("PlayerOne") == 0)
         {
             if (Input.GetButton("OneLeft"))
             {
-                Animator animator = this.GetComponentInChildren<Animator>();
                 movement.MoveLeft();
                 animator.SetBool("Moving", true);
                 animator.transform.localScale = new Vector3(-1, 1, 1);
             }
             if (Input.GetButton("OneRight"))
             {
-                Animator animator = this.GetComponentInChildren<Animator>();
                 movement.MoveRight();
                 animator.SetBool("Moving", true);
                 animator.transform.localScale = new Vector3(1, 1, 1);
             }
             if ((!Input.GetButton("OneLeft") && !Input.GetButton("OneRight")) || !movement.isGrounded)
             {
-                this.GetComponentInChildren<Animator>().SetBool("Moving", false);
+                animator.SetBool("Moving", false);
             }
             if (Input.GetButtonDown("OneJump") && movement.isGrounded)
             {
@@ -34,33 +36,31 @@ public class PlayerController : MonoBehaviour
             if (Input.GetButtonDown("OneDuck") && movement.isGrounded)
             {
                 movement.Duck();
-                this.transform.Find("SpriteAnchor/Sprite").transform.localScale = new Vector3(1.0f, 0.5f, 1.0f);
+                this.transform.Find(SpritePath).transform.localScale = new Vector3(1.0f, 0.5f, 1.0f);
             }
             if (Input.GetButtonUp("OneDuck"))
             {
                 movement.Stand();
-                this.transform.Find("SpriteAnchor/Sprite").transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+                this.transform.Find(SpritePath).transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
             }
         }
         else if (movement.tag.CompareTo("PlayerTwo") == 0)
         {
             if (Input.GetButton("TwoLeft"))
             {
-                Animator animator = this.GetComponentInChildren<Animator>();
                 movement.MoveLeft();
                 animator.SetBool("Moving", true);
                 animator.transform.localScale = new Vector3(-1, 1, 1);
             }
             if (Input.GetButton("TwoRight"))
             {
-                Animator animator = this.GetComponentInChildren<Animator>();
                 movement.MoveRight();
                 animator.SetBool("Moving", true);
                 animator.transform.localScale = new Vector3(1, 1, 1);
             }
             if ((!Input.GetButton("TwoLeft") && !Input.GetButton("TwoRight")) || !movement.isGrounded)
             {
-                this.GetComponentInChildren<Animator>().SetBool("Moving", false);
+                animator.SetBool("Moving", false);
             }
             if (Input.GetButtonDown("TwoJump") && movement.isGrounded)
             {
@@ -69,12 +69,12 @@ public class PlayerController : MonoBehaviour
             if (Input.GetButtonDown("TwoDuck") && movement.isGrounded)
             {
                 movement.Duck();
-                this.transform.Find("SpriteAnchor/Sprite").transform.localScale = new Vector3(1.0f, 0.5f, 1.0f);
+                this.transform.Find(SpritePath).transform.localScale = new Vector3(1.0f, 0.5f, 1.0f);
             }
             if (Input.GetButtonUp("TwoDuck"))
             {
                 movement.Stand();
-                this.transform.Find("SpriteAnchor/Sprite").transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+                this.transform.Find(SpritePath).transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
             }
         }
     }
