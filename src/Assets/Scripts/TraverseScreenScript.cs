03cc779afbc592398b73e88b95c6c5755b52cebf
kkdaman <kkdaman@hotmail.com>
2015-01-25 05:11:44 -0600

    Added the crappiest Title screen ever that transitions to level 1 After pressing enter

--- src/Assets/Scripts/TraverseScreenScript.cs
+++ src/Assets/Scripts/TraverseScreenScript.cs
@@ -5,13 +5,27 @@ public class TraverseScreenScript : MonoBehaviour {
 
     public string sceneToLoad;
 
-    void OnGUI()
-    {
-        GUI.Label(new Rect(Screen.width / 2 - 5, Screen.height - 8, 300, 300), "Current Scene: " + (Application.loadedLevelName));
+    //void OnGUI()
+    //{
+    //    GUI.Label(new Rect(Screen.width / 2 - 5, Screen.height - 8, 300, 300), "Current Scene: " + (Application.loadedLevelName));
 
-        if (GUI.Button(new Rect(Screen.width / 2 - 5, Screen.height - 5, 300, 300), "Load Scene " + sceneToLoad))
+    //    if (GUI.Button(new Rect(Screen.width / 2 - 5, Screen.height - 5, 300, 300), "Load Scene " + sceneToLoad))
+    //    {
+    //        Application.LoadLevel(sceneToLoad);
+    //    }
+    //}
+
+    void Start()
+    {
+        print("Current Scene: " + Application.loadedLevelName);
+    }
+
+    void Update()
+    {
+        if (Input.GetButton("Submit"))
         {
-            Application.LoadLevel(sceneToLoad);
+            print("Load Scene " + sceneToLoad);
+            Application.LoadLevel(sceneToLoad); 
         }
     }
 }
